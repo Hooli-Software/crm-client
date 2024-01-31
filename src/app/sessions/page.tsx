158c8d4b10f0c55cd94390ac0215ad0e3072b5a5
LEKS <ilhanchanel20002@gmail.com>
2024-01-31 19:36:29 +0300

    Session feed: First try

--- src/app/sessions/page.tsx
+++ src/app/sessions/page.tsx
@@ -1,5 +1,37 @@
+"use client"
+import {
+    Box,
+    Card,
+    ScrollArea,
+    TextArea,
+    Text
+} from "@radix-ui/themes"
+
+import css from "./page.module.scss"
+
+
 export default function SessionFeedPage() {
-    return <>
-        Session feed page
-    </>
+    return <Box className={css.wrapper}>
+        <Box className={css.container}>
+            <Box className={css.feed} py='3'>
+                <ScrollArea scrollbars="vertical" className={css.feedSignals}>
+                    <Box mx='3'>
+                        <Text>
+                            Session started at 12:02pm
+                        </Text>
+                    </Box>
+                    {
+                        Array.from(Array(9)).map((_, index)=>
+                                <Card key={index} mx="3" mt="3">
+                                    <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis dolorum dolorem, iure suscipit tempora.</Text>
+                                </Card>
+                            )
+                    }
+                </ScrollArea>
+                <Box p="3">
+                    <TextArea placeholder="Reply to comment…" />
+                </Box>
+            </Box>
+        </Box>
+    </Box> 
 }

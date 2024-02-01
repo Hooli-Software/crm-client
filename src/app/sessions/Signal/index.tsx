89fad264ffb4e9a8a68e383ab59495959a8b8b16
LEKS <ilhanchanel20002@gmail.com>
2024-02-01 19:28:01 +0300

    New component: Signal

---
+++ src/app/sessions/Signal/index.tsx
@@ -0,0 +1,67 @@
+import {
+    Box,
+    Flex,
+    Text
+} from "@radix-ui/themes"
+import { PauseIcon, PaperPlaneIcon, HandIcon, RocketIcon } from "@radix-ui/react-icons"
+
+import css from './index.module.scss'
+
+
+export type SignalType = 'start'| 'finish' | 'regular' | 'break'
+
+export interface ISignal {
+    content: string
+    time: string
+    type: SignalType
+}
+
+
+export default function Signal(props: ISignal) {
+    return <Flex m='3' mb='5' align='center' gap='4'>
+        <Box>
+            {
+                props.type === 'start' &&
+                    <RocketIcon className={css.iconType} />
+            }
+            {
+                props.type === 'finish' &&
+                    <HandIcon className={css.iconType} />
+            }
+            {
+                props.type === 'regular' &&
+                    <PaperPlaneIcon className={css.iconType} />
+            }
+            {
+                props.type === 'break' &&
+                    <PauseIcon className={css.iconType} />
+            }
+        </Box>
+        <Box>
+            <Text as="p" mb='1' color="gray">
+                {props.time}
+            </Text>
+            {
+                props.type === 'start' &&
+                <Text color="green">
+                    Session started - &nbsp;
+                </Text>
+            }
+            {
+                props.type === 'finish' &&
+                <Text color="green">
+                    Session ended - &nbsp;
+                </Text>
+            }
+            {
+                props.type === 'break' &&
+                <Text color="green">
+                    Break - &nbsp;
+                </Text>
+            }
+            <Text>
+                {props.content}
+            </Text>
+        </Box>
+    </Flex>
+}
--- src/app/sessions/page.tsx
+++ src/app/sessions/page.tsx
@@ -2,13 +2,15 @@
 import { useRef, useEffect } from "react"
 import {
     Box,
-    Card,
+    IconButton,
+    Flex,
     ScrollArea,
-    TextArea,
-    Text
+    TextField,
 } from "@radix-ui/themes"
+import { ChatBubbleIcon, PaperPlaneIcon } from "@radix-ui/react-icons"
 
 import css from "./page.module.scss"
+import Signal from './Signal'
 
 
 export default function SessionFeedPage() {
@@ -21,24 +23,47 @@ export default function SessionFeedPage() {
     return <Box className={css.wrapper}>
         <Box className={css.container}>
             <Box className={css.feed} py='3'>
-                <ScrollArea mt='3' ref={feedSignals} scrollbars="vertical" className={css.feedSignals}>
-                    <Box mx='3'>
-                        <Text>
-                            Session started at 12:02pm
-                        </Text>
-                    </Box>
-                    {
-                        Array.from(Array(9)).map((_, index)=>
-                                <Card key={index} mx="3" mt="3">
-                                    <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis dolorum dolorem, iure suscipit tempora.</Text>
-                                </Card>
+                <ScrollArea mt='3' ref={feedSignals} scrollbars="vertical" size='1' className={css.feedSignals}>
+                    <Box pr='3'>
+                        <Signal
+                            time='10:00'
+                            type='start'
+                            content='Надеюсь всё хорошо пройдёт!'
+                        />
+                        {
+                            Array.from(Array(9)).map((_, index) =>
+                                <Signal
+                                    key={index}
+                                    time='10:00'
+                                    type='regular'
+                                    content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ducimus dolorum possimus ullam assumenda veniam cumque recusandae nesciunt? Assumenda ratione, nisi temporibus aliquam esse similique natus praesentium repellat veritatis rem.'
+                                />
                             )
-                    }
+                        }
+                        <Signal
+                            time='10:00-12:00'
+                            type='break'
+                            content='Пойду посмотрю в потолок и вернусь!'
+                        />
+                        <Signal
+                            time='10:00'
+                            type='finish'
+                            content='Славный был день. Хорошо поработал!'
+                        />
+                    </Box>
                 </ScrollArea>
-                <Box p="3">
-                    <TextArea placeholder="Reply to comment…" />
-                </Box>
+                <Flex p="3" gap='2' className={css.panelInput}>
+                    <TextField.Root className={css.panelInputField}>
+                        <TextField.Slot>
+                            <ChatBubbleIcon height="16" width="16" />
+                        </TextField.Slot>
+                        <TextField.Input placeholder="Signal…" />
+                    </TextField.Root>
+                    <IconButton>
+                        <PaperPlaneIcon />
+                    </IconButton>
+                </Flex>
             </Box>
         </Box>
-    </Box> 
+    </Box>
 }

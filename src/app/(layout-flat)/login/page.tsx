ef1c727ffb4ad15463524f3b82924aa3c5b3b461
LEKS <ilhanchanel20002@gmail.com>
2024-02-17 01:13:23 +0300

    feature: new page - Login

---
+++ src/app/(layout-flat)/login/page.tsx
@@ -0,0 +1,66 @@
+'use client'
+import {
+    Box,
+    Button,
+    Card,
+    Flex,
+    Kbd,
+    TextField,
+    Text,
+    Link
+} from '@radix-ui/themes'
+
+import css from './index.module.scss'
+
+
+export default function LoginPage() {
+
+    return <Box className={css.wrapper} p='3' py='9'>
+        <Text as='p' mb='3'>If user logged in before:</Text>
+        <Card mb='5'>
+            <Box p='3'>
+                <Flex direction='column' gap='4'>
+                    <Text>Hello, <Kbd>lama</Kbd> please enter your password.</Text>
+                    <TextField.Input
+                        size='3'
+                        type='password'
+                        placeholder="Enter your email"
+                    />
+                    <Button
+                        variant='classic'
+                        color='green'
+                        size='3'
+                        // mt='3'
+                    >
+                        Press <Kbd color='red'>F</Kbd> to pay respect
+                    </Button>
+                    <Link>That's not me!</Link>
+                </Flex>
+            </Box>
+        </Card>
+        <Text as='p' mb='3'>If user is new:</Text>
+        <Card>
+            <Box p='3'>
+                <Flex direction='column' gap='4'>
+                    <TextField.Input
+                        size='3'
+                        placeholder="Enter your username"
+                    />
+                    <TextField.Input
+                        size='3'
+                        type='password'
+                        placeholder="Enter your email"
+                    />
+                    <Button
+                        variant='classic'
+                        color='green'
+                        size='3'
+                        // mt='3'
+                    >
+                        Press <Kbd>F</Kbd> to pay respect
+                    </Button>
+                </Flex>
+            </Box>
+        </Card>
+    </Box>
+}

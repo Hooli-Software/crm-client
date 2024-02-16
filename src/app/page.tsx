762a39c1dcd21c9936c4e3bbd99b639003703873
LEKS <ilhanchanel20002@gmail.com>
2024-02-17 01:16:19 +0300

    fix: updated main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,14 +9,18 @@ import {
 export default function Home() {
   return (
     <main>
-      <Container p='3' my='5'>
+      <Container p='3' my='5' size='1'>
         <Heading size='6'>
           Hello, Hooli Software!
         </Heading>
         <ul>
           <li><Link href='/sessions/'>Session list page</Link></li>
           <li><Link href='/sessions/1/'>Session detail page</Link></li>
-          <li><Link href='/sessions/1/feed/'>Session feed page</Link></li>
+          <li><Link href='/sessions/feed/1/'>Session feed page</Link></li>
+          <li><Link href='/customers/'>Customer list page</Link></li>
+          <li><Link href='/projects/'>Project list page</Link></li>
+          <li><Link href='/orders/'>Order list page</Link></li>
+          <li><Link href='/transactions/'>Transaction list page</Link></li>
         </ul>
       </Container>
     </main>
